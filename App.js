e1e3e44c36c3399febb4b83d6cb9ac31a5d36784
romkravets <romann.kravets@gmail.com>
2020-07-24 13:38:06 +0300

    appointment

--- App.js
+++ App.js
@@ -1,21 +1,137 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, SectionList } from 'react-native';
+import  {SectionTitle } from './components';
+import Appointment from './components/Appointment';
+import styled from 'styled-components/native';
+
+const DATA = [
+    {
+        title: "24 липня",
+        data: [
+            {
+                time: '15:30',
+                diagnosis: 'нарощення',
+                active: true,
+                user: {
+                    fullname: 'Анжела Матиева',
+                    avatar: 'https://reactnative.dev/img/tiny_logo.png',
+                }
+            },
+            {
+                time: '15:30',
+                diagnosis: 'відновлення',
+                user: {
+                    fullname: 'Анжела Матиева',
+                    avatar: 'https://reactnative.dev/img/tiny_logo.png',
+                }
+            },
+            {
+                time: '15:30',
+                diagnosis: 'відновлення',
+                user: {
+                    fullname: 'Анжела Матиева',
+                    avatar: 'https://reactnative.dev/img/tiny_logo.png',
+                }
+            },
+            {
+                time: '15:30',
+                diagnosis: 'відновлення',
+                user: {
+                    fullname: 'Анжела Матиева',
+                    avatar: 'https://reactnative.dev/img/tiny_logo.png',
+                }
+            },
+            {
+                time: '15:30',
+                diagnosis: 'відновлення',
+                user: {
+                    fullname: 'Анжела Матиева',
+                    avatar: 'https://reactnative.dev/img/tiny_logo.png',
+                }
+            },
+            {
+                time: '15:30',
+                diagnosis: 'відновлення',
+                user: {
+                    fullname: 'Анжела Матиева',
+                    avatar: 'https://reactnative.dev/img/tiny_logo.png',
+                }
+            },
+        ]
+    },
+    {
+        title: "25 липня",
+        data: [
+            {
+                time: '15:30',
+                diagnosis: 'нарощення',
+                user: {
+                    fullname: 'Анжела Матиева',
+                    avatar: 'https://reactnative.dev/img/tiny_logo.png',
+                }
+            },
+            {
+                time: '15:30',
+                diagnosis: 'відновлення',
+                user: {
+                    fullname: 'Анжела Матиева',
+                    avatar: 'https://reactnative.dev/img/tiny_logo.png',
+                }
+            },
+            {
+                time: '15:30',
+                diagnosis: 'відновлення',
+                user: {
+                    fullname: 'Анжела Матиева',
+                    avatar: 'https://reactnative.dev/img/tiny_logo.png',
+                }
+            },
+            {
+                time: '15:30',
+                diagnosis: 'відновлення',
+                user: {
+                    fullname: 'Анжела Матиева',
+                    avatar: 'https://reactnative.dev/img/tiny_logo.png',
+                }
+            },
+            {
+                time: '15:30',
+                diagnosis: 'відновлення',
+                user: {
+                    fullname: 'Анжела Матиева',
+                    avatar: 'https://reactnative.dev/img/tiny_logo.png',
+                }
+            },
+            {
+                time: '15:30',
+                diagnosis: 'відновлення',
+                user: {
+                    fullname: 'Анжела Матиева',
+                    avatar: 'https://reactnative.dev/img/tiny_logo.png',
+                }
+            },
+        ]
+    },
+];
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+        <SectionList
+            sections={DATA}
+            keyExtractor={(item, index) => item + index}
+            renderItem={({ item }) =>  <Appointment {...item} />}
+            renderSectionHeader={({ section: { title } }) => (
+                <SectionTitle>{title}</SectionTitle>
+            )}
+        />
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+  flex: 1;
+  margin-top: 30px;
+`;
+
+
